e2843f50e8b40f17c1aa46d3e3333e8ab7e755ab
Daffa Hanifisyafiq <dandroid36@gmail.com>
2025-09-09 23:38:00 +0700

    chore: add prettier and tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 const Home = () => {
-    return (
-        <main className="p-10">
-            <h1 className="text-4xl font-bold">Kos Ibu Ami Petarukan</h1>
-            <p className="mt-4 text-lg">Nyaman, strategis, dan harga terjangkau.</p>
-        </main>
-    );
-};
+  return (
+    <main className='p-10'>
+      <h1 className='text-4xl font-bold'>Kos Ibu Ami Petarukan</h1>
+      <p className='mt-4 text-lg'>Nyaman, strategis, dan harga terjangkau.</p>
+    </main>
+  )
+}
 
-export default Home;
+export default Home
